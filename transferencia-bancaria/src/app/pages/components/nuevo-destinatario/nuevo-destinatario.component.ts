1c9a4e7de8fc94b76978d67e3f758f3a317a7780
Carlos Lott <celotts@gmail.com>
2021-11-07 22:05:55 -0300

    prueba

--- transferencia-bancaria/src/app/pages/components/nuevo-destinatario/nuevo-destinatario.component.ts
+++ transferencia-bancaria/src/app/pages/components/nuevo-destinatario/nuevo-destinatario.component.ts
@@ -1,37 +1,35 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { BancosExternosService } from '../../service/bancos-externos.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormControl, FormGroup } from "@angular/forms";
+import { BancosExternosService } from "../../service/bancos-externos.service";
 
 @Component({
-  selector: 'app-nuevo-destinatario',
-  templateUrl: './nuevo-destinatario.component.html',
-  styleUrls: ['./nuevo-destinatario.component.scss'],
+	selector: "app-nuevo-destinatario",
+	templateUrl: "./nuevo-destinatario.component.html",
+	styleUrls: ["./nuevo-destinatario.component.scss"]
 })
 export class NuevoDestinatarioComponent implements OnInit {
-  Bancos: any = [];
+	Bancos: any = [];
 
-  tipoCuentas = [];
+	tipoCuentas = [];
 
-  destinatario = new FormGroup({
-    rut: new FormControl(''),
-    nombre: new FormControl(''),
-    correo: new FormControl(''),
-    telefono: new FormControl(''),
-    banco: new FormControl(''),
-    tipoCuenta: new FormControl(''),
-    numeroCuenta: new FormControl(''),
-  });
-  constructor(
-    public fb: FormBuilder,
-    private bancoExternoService: BancosExternosService
-  ) {}
+	destinatario = new FormGroup({
+		rut: new FormControl(""),
+		nombre: new FormControl(""),
+		correo: new FormControl(""),
+		telefono: new FormControl(""),
+		banco: new FormControl(""),
+		tipoCuenta: new FormControl(""),
+		numeroCuenta: new FormControl("")
+	});
+	constructor(public fb: FormBuilder, private bancoExternoService: BancosExternosService) {}
 
-  async ngOnInit() {
-    await this.bancoExternoService.getTipoCuenta().then((res) => {
-      this.tipoCuentas = res as any;
-    });
-    await this.bancoExternoService.obtenerBancosExternos().then((res) => {
-      this.Bancos = res.data;
-    });
-  }
+	async ngOnInit() {
+		console.log(222);
+		await this.bancoExternoService.getTipoCuenta().then(res => {
+			this.tipoCuentas = res as any;
+		});
+		await this.bancoExternoService.obtenerBancosExternos().then(res => {
+			this.Bancos = res.data;
+		});
+	}
 }
